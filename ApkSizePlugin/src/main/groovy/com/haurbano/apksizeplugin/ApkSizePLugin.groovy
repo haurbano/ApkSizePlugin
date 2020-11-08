1fed045e7704848b436f1c90c0805adb4bcb5e59
Hamilton Urbano <haurbano.dev@gmail.com>
2020-11-07 22:32:27 -0500

    Task in recognized in the app module but there is a problem when the task is going to run

---
+++ ApkSizePlugin/src/main/groovy/com/haurbano/apksizeplugin/ApkSizePLugin.groovy
@@ -0,0 +1,31 @@
+package com.haurbano.apksizeplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class APKSizePlugin implements Plugin<Project> {
+    static def formatSize(fileSizeInBytes) {
+        def fileSizeInKB = (fileSizeInBytes / 1024).round(2)
+        def fileSizeInMB = (fileSizeInKB / 1024).round(2)
+        if (fileSizeInKB > 1000) {
+            return "$fileSizeInMB MB"
+        } else {
+            return "$fileSizeInKB KB"
+        }
+    }
+
+    @Override
+    void apply(Project target) {
+        target.task('apkSize') {
+            doLast {
+                android.applicationVariants.all { variant ->
+                    variant.outputs.each { output ->
+                        def apkSize = output.outputFile.length()
+                        def readableSize = formatSize(apkSize)
+                        println("$variant.name Apk Size: $readableSize")
+                    }
+                }
+            }
+        }
+    }
+}
